e7f0f0ecfcd048f9ad647c4ea3c946bc41776818
rasmontas <rimantas.asmontas@gmail.com>
2023-08-30 18:58:38 +0300

    Initial commit

---
+++ app/src/main/java/lt/rimas/notes/MainActivity.java
@@ -0,0 +1,59 @@
+package lt.rimas.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import lt.rimas.notes.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "my_notes_main_activity";
+    private ActivityMainBinding binding;
+    private ArrayAdapter<Note> adapter;
+    private ArrayList<Note> notes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setUpListView();
+        setUpListViewClickListener();
+    }
+
+    private void setUpListView() {
+        notes = new ArrayList<>();
+
+        notes.addAll(
+                UseCaseRepository.generateDummyNotes(25)
+        );
+
+        adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                notes
+        );
+
+        binding.notesListView.setAdapter(adapter);
+    }
+
+    private void setUpListViewClickListener() {
+        binding.notesListView.setOnItemClickListener(
+                (adapterView, view, position, l) -> {
+
+                    Log.i(TAG, "OnListItemClicked : " +  adapterView.getItemAtPosition(position));
+                    Log.i(TAG, "OnListItemClicked : " +  position);
+
+                }
+        );
+    }
+}
---
+++ app/src/main/java/lt/rimas/notes/Note.java
@@ -0,0 +1,72 @@
+package lt.rimas.notes;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Note {
+
+    private int id;
+    private String title;
+    private String description;
+    private LocalDateTime creationDate;
+    private LocalDateTime updateDate;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public Note() {
+
+    }
+
+    public Note(
+            int id,
+            String title,
+            String description
+    ) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.creationDate = LocalDateTime.now();
+        this.updateDate = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        this.updateDate = LocalDateTime.now();
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        this.updateDate = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "id: %s\nTitle: %s\nDescription:\n\t%s\n\t%s\n\t%s",
+                this.id,
+                this.title,
+                this.description,
+                this.creationDate.format(formatter),
+                this.updateDate.format(formatter)
+        );
+    }
+}
